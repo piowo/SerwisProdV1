1e94d4a611a8e807e4ccd069cde691f623a515a1
Piotrek <piotrek@piotrek.com>
2023-02-20 14:55:21 +0100

    Added services implementations

---
+++ SerwisProdV1/Services/Implementations/ModuleService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SerwisProdV1.Services.Interfaces;
+using SerwisProdV1.Models;
+using SerwisProdV1.ModelsDTO;
+
+namespace SerwisProdV1.Services.Implementations
+{
+   
+    public class ModuleService : IModuleService
+    {
+        private readonly CalculatorContext context;
+
+        public ModuleService(CalculatorContext context)
+        {
+            this.context = context;
+        }
+
+        public OperationSuccesDTO<Module> AddModule(Module module)
+        {
+            context.Module.Add(module);
+            context.SaveChanges();
+            return new OperationSuccesDTO<Module> { Message = "Success" };
+        }
+
+        public Module GetModuleByName(string moduleName)
+        {
+            return context.Module.Where(module => module.Name == moduleName).FirstOrDefault();
+        }
+
+        public OperationSuccesDTO<List<Module>> GetModules()
+        {
+            List<Module> modules = context.Module.ToList();
+            return new OperationSuccesDTO<List<Module>> { Message = "Success", Result = modules };
+        }
+
+        public OperationSuccesDTO<Module> DeleteModule(string name)
+        {
+            var module = context.Module.Where(m => m.Name == name).FirstOrDefault();
+            context.Module.Remove(module);
+            context.SaveChanges();
+            return new OperationSuccesDTO<Module> { Message = "Success" };
+        }
+
+        public OperationSuccesDTO<Module> UpdateModule(Module module)
+        {
+            var mod = context.Module.Where(m => m.Name == module.Name).FirstOrDefault();
+            mod.Name = module.Name;
+            mod.Price = module.Price;
+            mod.Weight = module.Weight;
+            mod.AssemblyTime = module.AssemblyTime;
+            mod.Code = module.Code;
+            mod.Description = module.Description;
+            context.SaveChanges();
+            return new OperationSuccesDTO<Module> { Message = "Success" };
+        }
+    }
+}
